b970f9e049a5e2748b2890c2d26f7c6f73d38497
Łukasz 'Cyber Killer' Korpalski <cyberkiller8@gmail.com>
2022-08-25 13:06:30 +0200

    day1 extra challenge

--- day01.py
+++ day01.py
@@ -9,3 +9,21 @@ def divide_or_square(num):
             return nummod
     except:
         print("Int or float required")
+
+def longest_value(dictionary):
+    try:
+        longest = ''
+        for item in dictionary.values():
+            if len(item) > len(longest):
+                longest = item
+        return longest
+    except:
+        print("Dictionary required")
+
+# dict for testing
+# fruits = {
+#     'first': 'one',
+#     'fruit': 'apple',
+#     'color': 'green',
+#     'short': 'aaa'
+# }
